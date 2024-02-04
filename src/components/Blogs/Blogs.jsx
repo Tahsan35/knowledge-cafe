cf03120b409def2622b5564e342727ba2024434f
Tahsan <tahsanknmas14@gmail.com>
2024-02-04 20:22:25 +0600

    complete the project

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark, handleMarkAsRead }) => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
@@ -8,11 +10,25 @@ const Blogs = () => {
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
+
   return (
     <div className="md:w-2/3">
-      <h2>this is blog:{blogs.length}</h2>
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          handleAddToBookmark={handleAddToBookmark}
+          handleMarkAsRead={handleMarkAsRead}
+        ></Blog>
+      ))}
     </div>
   );
+};
+
+Blogs.propTypes = {
+  handleAddToBookmark: PropTypes.func,
+  handleMarkAsRead: PropTypes.func,
 };
 
 export default Blogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import img from "..//../assets/profile.png";
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 mx-4 border-b-2">
+    <header className="flex justify-between items-center p-4 border-b-2 max-w-7xl mx-auto">
       <h3 className="text-4xl font-bold">Knowledge Cafe</h3>
       <img src={img} alt="" />
     </header>
